7cb11ef3637dd1e6208cd2fa13c0cab71501fff5
Adynho1991 <ban.adrian1@yahoo.com>
2026-08-12 19:35:27 +0300

    Add service worker with caching and notification handling
    Implement service worker for caching and notifications.

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "planner-v6";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.matchAll({
+      type: "window",
+      includeUncontrolled: true
+    }).then(clientList => {
+      if (clientList.length) {
+        return clientList[0].focus();
+      }
+
+      return clients.openWindow("./");
+    })
+  );
+});
